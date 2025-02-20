52b0af32249dd280e119223267ecd0e00b67e3cf
weizhoublue <weizhou.lan@daocloud.io>
2025-02-20 22:27:18 +0800

    d
    Signed-off-by: weizhoublue <weizhou.lan@daocloud.io>

--- test/scripts/subnet.sh
+++ test/scripts/subnet.sh
@@ -76,38 +76,3 @@ spec:
     enablePxe: true
     enableZtp: true
 EOF
-
-#-----------
-
-NAME=net-invalid-nic
-CIDR=192.168.99.0/24
-IP_RANGE=192.168.99.100-192.168.99.200
-GATEWAY=192.168.99.1
-DNS=192.168.99.2
-INT_NAME=eth99
-INT_VLAN_ID=99
-INT_IPV4=192.168.99.3/24
-cat <<EOF | kubectl apply -f -
-apiVersion: topohub.infrastructure.io/v1beta1
-kind: Subnet
-metadata:
-  name: ${NAME}
-spec:
-  ipv4Subnet:
-    subnet: "${CIDR}"
-    ipRange: "${IP_RANGE}"
-    gateway: "${GATEWAY}"
-    dns: "${DNS}"
-  interface:
-    interface: "${INT_NAME}"
-    vlanId: ${INT_VLAN_ID}
-    ipv4: "${INT_IPV4}"
-  feature:
-    enableSyncEndpoint:
-      dhcpClient: true
-      defaultClusterName: cluster3
-      endpointType: hoststatus
-    enableBindDhcpIP: true
-    enablePxe: true
-    enableZtp: true
-EOF
